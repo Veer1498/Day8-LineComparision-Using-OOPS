9ddc06a452d246871983e0d52d72fb204d4b7d05
Veer1498 <singavarapuveerendra@gmail.com>
2022-06-28 23:47:53 +0530

    Welcome Screen

---
+++ src/oopsproblems/LineComparision.java
@@ -0,0 +1,10 @@
+package oopsproblems;
+
+public class LineComparision {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparision Computation Program");
+	}
+
+}
